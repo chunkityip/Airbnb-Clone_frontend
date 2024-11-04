b32aca5cb94c2fcade5d9fc4289bdf59d1000c37
Chunkit Yip <chunkit.yip@peopleshores.com>
2024-11-04 16:16:46 -0500

    style : add scss

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,11 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideAnimations } from '@angular/platform-browser/animations'; // Ensure you import this
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideAnimations(),  // Add provideAnimations here
+    provideRouter(routes)  // Your route provider
+  ]
 };
