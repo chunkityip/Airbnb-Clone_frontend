4f12d198928e0eee3fe6bab7b20f5966b3515b2e
Chunkit Yip <chunkit.yip@peopleshores.com>
2024-11-06 15:34:09 -0500

    feat: implement navbar and footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,15 @@ import { RouterOutlet } from '@angular/router';
 import {FaIconLibrary, FontAwesomeModule} from "@fortawesome/angular-fontawesome";
 import { ButtonModule} from "primeng/button";
 import { fontAwesomeIcons } from './shared/font-awesome-icons';
+import {NavbarComponent} from "./layout/navbar/navbar.component";
+import {FooterComponent} from "./layout/footer/footer.component";
+import {ToastModule} from "primeng/toast";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet , ButtonModule , FontAwesomeModule],
+  imports: [RouterOutlet, ButtonModule, FontAwesomeModule, NavbarComponent, FooterComponent, ToastModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
